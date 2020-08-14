0334186e9b8027b51bc8cb71d5232a38a51ef92e
Sarah ALIBAY <alibaymand@eisti.eu>
2020-08-14 09:22:46 +0200

    Add code challenge answer 3

--- 5 - Code Challenge/app.js
+++ 5 - Code Challenge/app.js
@@ -3,6 +3,13 @@ const app = express();
 
 const PORT = process.env.PORT || 4001;
 
+// 3. get routes
+const sausageTypes = ['bratwurst', 'andouille', 'chorizo', 'boudin', 'kielbasa'];
+
+app.get('/sausages', (req, res, next) => {
+  res.send(sausageTypes)
+})
+
 // 2. Add your code below:
 app.listen(PORT, () => {
   console.log(`App is listening on Port ${PORT}`)
